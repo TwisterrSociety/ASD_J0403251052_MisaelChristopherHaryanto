283b671fc89f90a208f73708ef900ddba466285c
1 3 3 7 <olahudia@gmail.com>
2026-03-04 16:16:12 +0700

    Praktikum-6

--- Praktikum-6/Materi/praktikum6-materi2.py
+++ Praktikum-6/Materi/praktikum6-materi2.py
@@ -5,13 +5,27 @@
 #================================================
 
 #================================================
-# Insertion Sort (Ascending)
+# Insertion Sort dengan tracing
 #================================================
 
 def insertion_sort(data):
+    #melihat data awal
+    print("Data Awal: ", data)
+    print("="*50)
+
     #Loop mulai dari index ke 1
     for i in range(1, len(data)):
+
+
         key = data[i] #simpan nilai yang disisipkan
+
+
+        print("Iterasi ke-", i)
+        print("Key yang disisipkan: ", key)
+        print("Bagian kiri (sudah terurut): ", data[:i])
+        print("Bagian kanan (belum terurut): ", data[i:])
+
+
 
         j = i - 1 #index elemen sebelum key
         #Pindahkan elemen yang lebih besar dari key ke posisi setelahnya
@@ -19,7 +33,10 @@ def insertion_sort(data):
             data[j + 1] = data[j] #geser elemen ke kanan
             j -= 1
         data[j + 1] = key #tempatkan
+        print("Setelah disisipkan: ", data)
+        print("="*50)
     return data
+
 
 angka = [12, 11, 13, 5, 6, 7]
 print("Hasil Insertion Sort (Ascending):", insertion_sort(angka))
